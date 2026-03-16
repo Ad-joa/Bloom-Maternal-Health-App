1c57793f2212786726498d994565567f5dd04ab6
Diapim Fortune Akpene <fadiapim@st.ug.edu.gh>
2026-03-16 19:36:10 +0000

    chore: remove README.md and .env.example files.

--- backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user import User
-from .health import PregnancyProfile, HealthContent, SymptomLog, AntenatalVisit
-
-# This allows for easy importing of all models
-__all__ = ["User", "PregnancyProfile", "HealthContent", "SymptomLog", "AntenatalVisit"]
